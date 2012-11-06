28b02e30169934158b7ce73ea224188fe8bbf019
Derrick Cheng <dcheng090@gmail.com>
2012-11-05 23:58:58 -0800

    adding factual restaurant data gathering

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Installation
 The Twitter code uses the `tweepy` library. Install it with `[sudo] pip install tweepy`.
 
 The Instagram code makes use of the `python-instagram` library. `[sudo] pip install python-instagram`
+
+The Factual code makes use of the 'factual-api' from https://github.com/Factual/factual-python-driver. Install it with 'pip install factual-api' The results of my test are located in S3
---
+++ factual/get-restaurants.py
@@ -0,0 +1,35 @@
+from factual import Factual
+from factual_api_keys import KEY, SECRET
+factual = Factual(KEY,SECRET)
+
+if __name__ == "__main__":
+	limit=50
+	maximum=500
+	results =[]
+	query = factual.table("restaurants-us").filters({"$and":[{"region":"CA"},{"locality":"berkeley"}]}).include_count(True).limit(str(limit)).offset("0").sort("address:asc")
+	tot=query.total_row_count()
+	curOffset = limit
+	results= results+query.data()
+	while(curOffset<min(tot,maximum)):
+		query = factual.table("restaurants-us").filters({"$and":[{"region":"CA"},{"locality":"berkeley"}]}).include_count(True).limit(str(limit)).offset(str(curOffset)).sort("address:asc")
+		results = results+query.data()
+		curOffset=curOffset+limit
+	
+	# this is to get the rest of the restaurants
+	# reverse the query and get the ones we didn't get earlier
+	if (tot>maximum):
+		curOffset = 0
+		nextMax = tot - maximum 
+		while(curOffset<nextMax): 
+			if ((nextMax-curOffset)<50):
+				lim = (nextMax-curOffset) % limit
+			else:
+				lim = limit
+			query = factual.table("restaurants-us").filters({"$and":[{"region":"CA"},{"locality":"berkeley"}]}).include_count(True).limit(str(lim)).offset(str(curOffset)).sort("address:desc")
+			results = results + query.data()
+			curOffset=curOffset+lim
+	# print tot
+	# print len(results)
+	print results
+		
+	
